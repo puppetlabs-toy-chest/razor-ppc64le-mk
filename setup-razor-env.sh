e5fc0d721767a9585465e75edb3583e522de441d
Mick Tarsel <mjtarsel@linux.vnet.ibm.com>
2018-06-08 20:17:15 -0700

    Setup razor on currently running alpine instance
    a more modular script using bash functions to setup razor and then generate
    a initramfs that is PXE bootable. the goal for this script is to
    generate a tarball with vmlinuz and pxe-initramfs which can be placed on
    razor server. does not create a iso because everything is included in
    the PXE bootable initramfs

---
+++ setup-razor-env.sh
@@ -0,0 +1,137 @@
+#!/bin/sh
+#Written by Mick Tarsel
+
+#Create the environment for razor to copy into a initramfs
+#Eventually this will be in a PXE boot-able image for currently running Alpine instace
+
+
+# where gems will exist for use by mk service
+GEM_DIR=./gems
+APK_DIR=./apks
+
+
+download_packages() {
+  apk del ruby ruby-dev
+apk update
+
+# build dependices from wiki and razor
+apk add alpine-sdk build-base apk-tools alpine-conf busybox fakeroot xorriso ruby ruby-dev
+
+# needed to build the razor-mk-agent.gem and convert gems to .apks
+gem install etc fpm facter rake bundler --no-document
+}
+
+create_apks_from_gems() {
+  # setup dir for building gems
+  mkdir -p $GEM_DIR/
+
+  # build custom razor-mk-agent.gem
+  bundle install
+  bundle exec rake build
+
+  # location of razor-mk-agent-008.gem
+  cp ./pkg/*.gem $GEM_DIR/
+
+  # local install facter gem
+  gem install facter --no-document -i $GEM_DIR/
+
+  # move all gems into one location
+  cp $GEM_DIR/facter/cache/*.gem $GEM_DIR/
+  #rm -rf ./facter #remove extra build dirs
+
+  #need to comment out a line to get working apk
+  fpm_file=$(find /usr/lib/ruby | grep apk.rb)
+  #line 255 #full_record_path = add_paxstring(full_record_path)
+  cp -f ./apk.rb $fpm_file
+
+  #build 2 apks from 2 gems
+  mkdir -p $APK_DIR/
+  cd $APK_DIR #where we will build apks
+  fpm -n facter -a ppc64le -s gem -t apk ../$GEM_DIR/facter-2.5.1.gem
+  fpm -n razor-mk-agent -a ppc64le -s gem -t apk ../$GEM_DIR/razor-mk-agent-008.gem
+
+  #sign the apks
+  #TODO where are apks built?
+ abuild-sign $APK_DIR/*.apk
+
+}
+
+install_custom_apks() {
+  apk add $APK_DIR/facter*.apk --allow-untrusted
+	apk add $APK_DIR/razor-mk-agent*.apk --allow-untrusted
+}
+
+setup_mk_service() {
+  #TODO Is this all the files we need for mk? what about ./lib/
+  # move shell scripts used by mk OpenRC service
+  mkdir -p /usr/local/bin
+  chmod +x ./bin/mk*
+	cp -R ./bin /usr/local/bin
+	chmod +x ./bin/mk*
+
+  cp ./mk /etc/init.d/
+}
+
+start_mk_service() {
+  rc-service add mk default
+  /etc/init.d/mk start
+}
+
+setup_pxe_boot() {
+  echo "/usr/share/udhcpc/default.script" > /etc/mkinitfs/features.d/dhcp.files
+   echo "kernel/net/packet/af_packet.ko" > /etc/mkinitfs/features.d/dhcp.modules
+   echo "kernel/fs/nfs/*" > /etc/mkinitfs/features.d/nfs.modules
+   echo 'features="ata base bootchart cdrom cramfs ext2 ext3 ext4 xfs floppy keymap kms raid scsi usb virtio squashfs network dhcp nfs"' > /etc/mkinitfs/mkinitfs.conf
+
+}
+
+generate_pxe_initramfs() {
+  pxe_dir=/root
+  mkinitfs -o $pxe_dir/pxe-initramfs
+  echo "initramfs in $pxe_dir"
+}
+
+check_kernel() {
+  supported_kernel="4.14.48-0-vanilla"
+  if [ $(uname -r) != $supported_kernel];then
+    echo "Please update kernel to at least $supported_kernel"
+    exit 1
+  fi
+}
+
+build_iso() {
+  # repo with build scripts
+  git clone https://github.com/alpinelinux/aports.git
+
+  # copy my custom razor profile
+  cp genapkovl-razor.sh ./aports/scripts
+  cp mkimg.razor.sh ./aports/scripts
+  cp mkimg.base.sh ./aports/scripts
+
+  cd aports/scripts
+
+  # where .iso will be once complete
+  mkdir -p ~/iso
+
+  # build the .iso for ppc64le using razor profile and output to ~/iso
+  # the edge repo has the kernel and other special apks so need to use it
+  sh mkimage.sh --tag main --outdir ~/iso --arch ppc64le --repository http://dl-cdn.alpinelinux.org/alpine/edge/main --profile razor
+  #mkimage -> mkimg.razor.sh -> genapkovl-razor.sh -> mkimg.base.sh
+}
+
+#TODO vmlinuz and pxe-initramfs in one place
+tar_microkernel(){
+  cp /boot/vmlinuz ./
+}
+
+#BEGIN EXECUTION
+check_kernel
+download_packages #setup repositories to install needed packages to build
+create_apks_from_gems #turn facter.gem and razor-mk-agent.gem into apks to use by Alpine
+install_custom_apks #install facter.apk and razor-mk-agent.apk
+setup_mk_service #move around executables used by mk service
+start_mk_service
+setup_pxe_boot #edit /etc/mkinitfs
+generate_pxe_initramfs
+#tar_microkernel #take vmlinuz and new pxe-initramfs and put in a tarball
+#build_iso #TODO is this needed or use build-iso.sh?
