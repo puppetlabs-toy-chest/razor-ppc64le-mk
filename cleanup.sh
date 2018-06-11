cd2646cefb0c949c8760e1420f95d3faefdf6e0d
Mick Tarsel <mjtarsel@linux.vnet.ibm.com>
2018-06-11 14:20:09 -0700

    cleanup is its own script

---
+++ cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+  echo ""
+  echo "Cleaning up..."
+  echo ""
+  #Will not remove pxe bootable configs or apk.rb for fpm
+  apk del ruby ruby-dev facter razor-mk-agent
+  rm -rf $BUILD_DIR/pkg
+  rm -rf $BUILD_DIR/gems
+  rm -rf $BUILD_DIR/apks
+  rm -rf $BUILD_DIR/PXE
+  rm -rf $BUILD_DIR/microkernel-ppc64le
+  rm /usr/local/bin/mk*
+  rm -rf /root/pxe-initramfs
+  /etc/init.d/mk stop
+  rm /etc/init.d/mk
