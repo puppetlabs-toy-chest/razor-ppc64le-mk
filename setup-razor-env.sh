118a6420205eacdbddc6642a22d1876f23952513
Mick Tarsel <mtarsel@gmail.com>
2018-07-17 09:51:16 -0700

    correctly pipe nameservers to resolv.conf
    changed > to >>

--- setup-razor-env.sh
+++ setup-razor-env.sh
@@ -202,8 +202,8 @@ build_apkovl_tar(){
   
   #setup DNS 
   echo > /etc/resolv.conf
-  echo "nameserver 8.8.8.8" > /etc/resolv.conf
-  echo "nameserver 8.8.4.4" > /etc/resolv.conf
+  echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+  echo "nameserver 8.8.4.4" >> /etc/resolv.conf
   lbu include /etc/resolv.conf
 
   #double check nothing is installed
